ef02e473c0309904b7bd40c362be54b0b1a2e311
Brendan Hawk <brendanhawk03@aol.com>
2025-01-15 07:55:17 -0500

    Initial Commit. JS, CSS, HTML files created and linked

---
+++ script.js
@@ -0,0 +1,7 @@
+/* Project Description:
+A recipe manager where users can 
+add, edit, and delete their favorite recipes.
+
+Save the recipes in localStorage, including details 
+like ingredients and preparation steps.
+*/
