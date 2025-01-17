b2ec71f566e43cffb1fbec21f3da9ba1c3a4fe96
Brendan Hawk <brendanhawk03@aol.com>
2025-01-16 19:16:09 -0500

    Add Recipe button and form

--- script.js
+++ script.js
@@ -5,3 +5,18 @@ add, edit, and delete their favorite recipes.
 Save the recipes in localStorage, including details 
 like ingredients and preparation steps.
 */
+
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+  }
+  
+  function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+  }
+
+
+  let recipeCol1 = document.getElementById("recipe-col-1")
+  let recipeCol2 = document.getElementById("recipe-col-2")
+  let recipeCol3 = document.getElementById("recipe-col-3")
+
+  
