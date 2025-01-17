217084d0a55737cd887c154655f887c72f51e323
Brendan Hawk <brendanhawk03@aol.com>
2025-01-17 08:46:01 -0500

    Starting on working through the form submit data  and pushing it to an obj

--- script.js
+++ script.js
@@ -19,4 +19,15 @@ function openForm() {
   let recipeCol2 = document.getElementById("recipe-col-2")
   let recipeCol3 = document.getElementById("recipe-col-3")
 
-  
+  let recipeIngredient1 = document.getElementById("recipe-ingredient-1")
+  let recipeIngredient2 = document.getElementById("recipe-ingredient-2")
+  let recipeIngredient3 = document.getElementById("recipe-ingredient-3")
+
+  let recipeList = {}
+
+/* Add btn doesnt have event listener */
+function addRecipe() {
+
+  /* im thinking of setting the form data = to a var. something like myForm.recipe-name */
+
+}
